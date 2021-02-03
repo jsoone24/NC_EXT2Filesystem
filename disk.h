23866bd7915bac78476215d118bdd4cbb41df291
Eunseo Choi <55284181+maychoi410@users.noreply.github.com>
2021-02-03 20:03:54 +0900

    Update disk.h

--- disk.h
+++ disk.h
@@ -9,7 +9,7 @@ typedef struct DISK_OPERATIONS
 	int		( *write_sector	)( struct DISK_OPERATIONS*, SECTOR, const void* );	//디스크에 쓰는 동작 연결 필요
 	SECTOR	numberOfSectors;	//섹터의 수
 	int		bytesPerSector;		//섹터당 바이트 수
-	void*	pdata;				//?????
+	void*	pdata;				//디스크 메모리 
 } DISK_OPERATIONS;				//디스크 동작에 관한 구조체
 
 #endif
