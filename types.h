2e9d4d8fc5921fad4f4b94497aea3aa929587ede
Sinseungmin <s474747@naver.com>
2021-02-03 14:08:22 +0900

    first

---
+++ types.h
@@ -0,0 +1,32 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+#define BYTE				unsigned char               
+#define WORD				unsigned short              
+#define DWORD				unsigned int                
+#define QWORD				unsigned long long int    
+#define SHORT				short                       
+#define USHORT				unsigned short           
+#define INT					int                       
+#define UINT				unsigned int              
+#define INT8				char                        
+#define UINT8				unsigned char               
+#define INT16				SHORT                       
+#define UINT16				USHORT                      
+#define INT32				INT                         
+#define UINT32				UINT                        
+#define INT64				long long int               
+#define UINT64				unsigned INT64              
+
+#endif
+
+/*
+BYTE, UNIT8				    unsigned char               //  1byte   /                0~255              /    
+WORD, USHORT, UNIT16		unsigned short              //  2byte   /               0~65535             /
+DWORD, UNIT, UNIT32			unsigned int                //  4byte   /          0 ~ 4,294,967,295        /
+QWORD, UNIT64				unsigned long long int      //  8byte   /   0 ~ 18,446,744,073,709,551,615  /
+SHORT, INT16				short                       //  2byte   /           ?32,768 ~ 32,767        /
+INT, INT32					int                         //  4byte   /  ?2,147,483,648 ~ 2,147,483,647   /
+INT8				        char                        //  1byte   /             -128 ~ 127            /
+INT64				        long long int               //  8byte   /   	?9,223,372,036,854,775,808 ~ 9,223,372,036,854,775,807
+*/
