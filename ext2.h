ad1b5741da3c28d303c441d4efeb5839539228dc
Sinseungmin <s474747@naver.com>
2021-02-04 19:08:34 +0900

    2021/02/04

---
+++ ext2.h
@@ -0,0 +1,177 @@
+#ifndef _FAT_H_
+#define _FAT_H_
+
+#include "common.h"
+#include "disk.h"
+#include <string.h>
+#include <sys/resource.h>
+#include <sys/types.h>
+#include <unistd.h>
+#define GNU_SOURCE"
+
+#include <string.h>
+
+#define EXT2_N_BLOCKS         	15				//직,간접 블록 개수
+#define	NUMBER_OF_SECTORS		( 4096 + 1 )	//섹터 수?
+#define	NUMBER_OF_GROUPS		2				//그룹 수?
+#define	NUMBER_OF_INODES		200				//아이노드 수?
+#define	VOLUME_LABLE			"EXT2 BY NC"	//볼륨 이름
+
+#define MAX_SECTOR_SIZE			1024			//최대 섹터 크기
+#define MAX_BLOCK_SIZE			1024			//최대 블록 크기
+#define MAX_NAME_LENGTH			256				//최대 이름 제한
+#define MAX_ENTRY_NAME_LENGTH	11				//최대 엔트리 이름 제한
+
+#define ATTR_READ_ONLY			0x01			//아이노드 필드 설정 시 사용되는 가ㅄ으로 예상됨
+#define ATTR_HIDDEN				0x02			//아이노드 필드 설정 시 사용되는 가ㅄ으로 예상됨
+#define ATTR_SYSTEM				0x04			//아이노드 필드 설정 시 사용되는 가ㅄ으로 예상됨
+#define ATTR_VOLUME_ID			0x08			//아이노드 필드 설정 시 사용되는 가ㅄ으로 예상됨
+#define ATTR_DIRECTORY			0x10			//아이노드 필드 설정 시 사용되는 가ㅄ으로 예상됨
+#define ATTR_ARCHIVE			0x20			//아이노드 필드 설정 시 사용되는 가ㅄ으로 예상됨
+#define ATTR_LONG_NAME			ATTR_READ_ONLY | ATTR_HIDDEN | ATTR_SYSTEM | ATTR_VOLUME_ID
+
+#define DIR_ENTRY_FREE			0xE5			//디렉토리 엔트리 관리시 필요한 필드인것 같다.
+#define DIR_ENTRY_NO_MORE		0x00			//디렉토리 엔트리 관리시 필요한 필드인것 같다.
+#define DIR_ENTRY_OVERWRITE		1				//디렉토리 엔트리 관리시 필요한 필드인것 같다.
+
+#define GET_INODE_GROUP(x)		((x) - 1)/( NUMBER_OF_INODES / NUMBER_OF_GROUPS )
+#define SQRT(x)  				( (x) * (x)  )
+#define TRI_SQRT(x)				( (x) * (x) * (x) )
+#define WHICH_GROUP_BLONG(x)	( ( (x) - 1)  / ( NUMBER_OF_INODES / NUMBER_OF_GROUPS )  )
+
+#define TSQRT(x) 				((x)*(x)*(x))
+#define GET_INODE_FROM_NODE(x) 	((x)->entry.inode)
+
+#ifdef _WIN32
+#pragma pack(push,fatstructures)
+#endif
+#pragma pack(1)
+
+typedef struct
+{
+	UINT32 max_inode_count;				//파일 시스템에서 샤용가능한 최대 아이노드 수
+	UINT32 block_count;					//파일 시스템 내의 전체 블록 수
+	UINT32 reserved_block_count;		//아무 대책없이 파일시스템이 꽉 차는 경우를 막기 위해 예약된 블록 영역
+	UINT32 free_block_count;			//비어있는 블록 수
+	UINT32 free_inode_count;			//비어있는 아이노드 수
+
+	UINT32 first_data_block;			//첫번째 블록, 즉 블록 그룹 0이 시작되는 블록
+	UINT32 log_block_size;				//블록 크기 지정하는 가ㅄ 0:1KB, 1:2KB, 2:4KB
+	UINT32 log_fragmentation_size;		//단편화 발생 시 기록되는 크기 0 1 2로 기록 위에와 크기 같음
+	UINT32 block_per_group;				//블록 그룹마다 블록이 몇개 있는지
+	UINT32 fragmentation_per_group;		//각 블록 그룹에 속한 단편화된 개수
+	UINT32 inode_per_group;				//각 블록그룹에 속한 아이노드 수
+	UINT16 magic_signature;				//슈퍼블록인지 확인하는 고유가ㅄ
+	UINT16 errors;						//에러 처리를 위한 플래그. 자세한 에러코드는 책 320쪽 참고 
+	UINT32 first_non_reserved_inode;	//예약되지 않은 아이노드의 첫번째 인덱스, 일반적으로 10개의 아이노드가 예약되어있음
+	UINT16 inode_structure_size;		//아이노드 구조체 크기
+
+	UINT16 block_group_number;			//현재 슈퍼블록을 포함하고 있는 블록 그룹 번호. 각 블록그룹마다 존재하는 슈퍼블록 원본인지 복사본인지 파악
+	UINT32 first_data_block_each_group;	//첫번째 데이터 블록 번호
+} EXT2_SUPER_BLOCK;						//super block 에 대한 구조체. 각 블록 그룹마다 있다. 총 1KB
+
+typedef struct
+{
+	UINT32 start_block_of_block_bitmap;	//블록 비트맵의 블록 번호. 그룹 디스크립터 테이블 크기가 고정되어 있지 않기 대문에 바로 다음에 위치하는 블록 비트맵 위치 나타냄
+	UINT32 start_block_of_inode_bitmap;	//아이노드 비트맵의 블록 번호. 블록 비트맵 위치 나타냄
+	UINT32 start_block_of_inode_table;	//아이노드 태이블의 블록 번호. 실제 아이노드 데이터가 담겨있는 블록이 시작되는 블록의 번호이다.
+	UINT32 free_blocks_count;			//블록 그룹내의 비어있는 블록 수. 데이터 저장 시 비어있는 블록이 많은 블록 그룹이 우선적으로 선택됨.
+	UINT32 free_inodes_count;			//블록 그룹내의 비어있는 아이노드 수
+	UINT16 directories_count;			//블록 그룹내에 생성된 디렉토리 수. 디렉토리에 데이터 저장시 되도록 부모 디렉터리와 같은 블록 그룹에 저장하려함. 그래서 이 디렉토리 수가 많을 수록
+										//디렉터리를 넣을 공간이 적다는 의미이기 때문에 저장시 이 개수가 적은 곳에 저장하게됨
+	BYTE padding[2];					//4바이트 단위로 정렬을 위한 패딩이다. directories_count는 2바이트로 4바이트 기준 저장 시 2바이트가 남는데 이 빈공간을 채움
+	BYTE reserved[12];					//예약된 영역. 32바이트를 맞추기 위해 12바이트를 패딩하기 위한 영역
+} EXT2_GROUP_DESCRIPTOR;				//모든 블록 그룹에 대한 디스크립터. 각 블록 그룹마다 저장되어있다. 하나의 디스크립터는 총 32바이트
+
+typedef struct
+{
+	UINT16  mode;						/* File mode 아이노드 타입 : 파일인지 디렉토리인지, 권한 등 책 331페이지 참고*/
+	UINT16  uid;						/* Low 16 bits of Owner Uid 소유자 식별자*/
+	UINT32  size; 						/* Size in bytes 파일크기를 바이트 단위로 나타냄*/
+	UINT32  atime;						/* Access time 마지막 접근 시간*/
+	UINT32  ctime;						/* Creation time - 생성시간*/
+	UINT32  mtime;						/* Modification time 파일을 마지막으로 변경한 시간*/
+	UINT32  dtime;						/* Deletion Time 파일이 삭제된 시간*/
+	UINT16  gid;  						/* Low 16 bits of Group Id 그룹 식별자*/
+	UINT16  links_count;				/* Links count 하드 링크 수*/
+	UINT32  blocks;     				/* Blocks count 파일 데이터를 저장하는데 필요한 블록 개수*/
+	UINT32  flags;      				/* File flags 파일 플래그 책 334페이지 참고, 아래 151번줄에 하위 13~16비트에 대한 갑ㅅ 정의 되어있음*/
+	UINT32  i_reserved1;				// OS dependent 1 -????? 특정 운영체제 정보라는데 실질적으로 사용되지 않는다고 함
+	UINT32  block[EXT2_N_BLOCKS];		/* Pointers to blocks 데이터 블록을 가리키는 포인터 배열 15개. 12개 직접블록 1개 간접 포인터, 1개 이중 간접 포인터 1개 3중 간접포인터*/
+	UINT32  generation;					/* File version (for NFS) 네트워크 파일시스템을 위한 파일 버전*/
+	UINT32  file_acl;  					/* File ACL 파이르이 확장 속성*/
+	UINT32  dir_acl;   					/* Directory ACL 디렉토리 접근 제어 리스트*/
+	UINT32  faddr;     					/* Fragment address 단편 주소 */
+	UINT32  i_reserved2[3];				// OS dependent 2 -????? 특정 운영체제 정보라는데 실질적으로 사용되지 않는다고 함
+} INODE; 								//아이노드 구조체. 크기는 슈퍼블록에 기록된 크기로 계산되지만 일반적으로 128바이트임
+
+//아이노드도 리스트로 되어있기 때문에 인덱스를 가짐. 이중에서 1 번은 사용불가능한 데이터 블록의 아이노드, 2 번은 루트 디렉터리의 아이노드 5번은 부트로더의 아이노드
+//6번은 삭제 불가 디렉터리의 아이노드 11번은 예약되지 않은 첫 번째 아이노드 의미
+
+typedef struct							
+{
+	UINT32 inode;						//이 디렉터리 엔트리가 가리키는 데이터의 아이노드 번호. 이 아이노드번호로 아이노드를 찾으면 파일인지 디렉터리인지 알 수 있음
+	BYTE name[11];						//파일 명의 길이
+	UINT32 name_len;					//파일 명
+	BYTE pad[13];						//아마 책에서 Record Length에 해당하는 것 같음 생성시 엔트리 크기를 4의 배수로 맞추기 위한 영역인듯
+} EXT2_DIR_ENTRY;						//디렉터리 엔트리로 디렉터리 내부에 어떤 데이터들이 저장되어 있는지 파악. 아마 연결리스트로 연결될거 같음
+
+#ifdef _WIN32
+#pragma pack(pop, fatstructures)
+#else
+#pragma pack()
+#endif
+
+typedef struct
+{
+	EXT2_SUPER_BLOCK		sb;			//슈퍼블럭을 가리킴
+	EXT2_GROUP_DESCRIPTOR	gd;			//그룹 디스크립터를 가리킴
+	DISK_OPERATIONS*		disk;		//디스크 동작에 대한 것 disk.h 에 있다.
+} EXT2_FILESYSTEM;						//이 구조체 하나가 블록 그룹 하나를 지칭 할 것으로 예상됨
+
+typedef struct
+{
+	UINT32 group;						//?????
+	UINT32 block;						//?????
+	UINT32 offset;						//?????
+} EXT2_DIR_ENTRY_LOCATION;				//디렉터리 엔트리 위치에 대한 구조체인듯 하다. 왜 필요할까?
+
+typedef struct
+{
+	EXT2_FILESYSTEM * fs;				//위의 구조체
+	EXT2_DIR_ENTRY entry;				//디렉터리 엔트리의 최상단 아니면 최하단을 가리키는것으로 예상
+	EXT2_DIR_ENTRY_LOCATION location;	//위의 구조체
+} EXT2_NODE;							//블록 그룹 하나를 관리하기 위한 객체인 것 같다. 이게 블록 그룹 개수 만큼 있을 것 같다. 배열이든 연결리스트로든
+
+#define SUPER_BLOCK 0					//슈퍼블럭의 블럭 위치. 항상 0번째임
+#define GROUP_DES  1					//그룹 디스크립터는 슈퍼블록 뒤에 항상 위치 그래서 1번
+#define BLOCK_BITMAP 2					//블록 비트맵은 그룹 디스크립터 바로 뒤에 위치 그래서 2번
+#define INODE_BITMAP 3					//아이노드 비트맵은 블록 비트맵 바로 뒤에 위치 그래서 3번
+#define INODE_TABLE(x) (4 + x)			//아이노드 테이블은 여러 블록에 있을 수 있기 때문에 4~ 번
+
+#define FILE_TYPE_FIFO               0x1000	//명명된 파이프?
+#define FILE_TYPE_CHARACTERDEVICE    0x2000	//문자 장치
+#define FILE_TYPE_DIR				 0x4000	//디렉터리
+#define FILE_TYPE_BLOCKDEVICE        0x6000	//블록 장치
+#define FILE_TYPE_FILE				 0x8000	//이건 뭘까? 332페이지에서는 정규파일이라고 함
+//332페이지 참고 아이노드의 File mode의 하위 13~16비트에 저장됨.
+
+int meta_read(EXT2_FILESYSTEM *, SECTOR group, SECTOR block, BYTE* sector);			//data_read랑 똑같이 생겼는데 뭘까? 이건 디스크에서 데이터를 읽어주는 함수이다.
+int meta_write(EXT2_FILESYSTEM * fs, SECTOR group, SECTOR block, BYTE* sector);		//data_write랑 똑같이 생김. 디스크에 데이터를 쓰는 함수이다.
+int data_read(EXT2_FILESYSTEM *, SECTOR group, SECTOR block, BYTE* sector);			//위랑 내용 같음
+int data_write(EXT2_FILESYSTEM * fs, SECTOR group, SECTOR block, BYTE* sector);		//위랑 내용 같음
+
+int ext2_format(DISK_OPERATIONS* disk);												//ext2파일 시스템으로 초기화 하는 함수
+int ext2_create(EXT2_NODE* parent, char* entryName, EXT2_NODE* retEntry);			//파일시스템에서 파일을 새로 생성할때 호출되는 함수
+int ext2_lookup(EXT2_NODE* parent, const char* entryName, EXT2_NODE* retEntry);		//entryName을 갖는 엔트리가 있는지 검색해 그 위치를 리턴
+int ext2_df(EXT2_FILESYSTEM* fs, unsigned int total, unsigned int used);
+int ext2_rmdir(EXT2_NODE* dir); 
+
+UINT32 expand_block(EXT2_FILESYSTEM * , UINT32 );									//????
+int fill_super_block(EXT2_SUPER_BLOCK * sb, SECTOR numberOfSectors, UINT32 bytesPerSector);										//메모리 어떤 곳에 슈퍼블록 내용을 채워넣는 함수
+int fill_descriptor_block(EXT2_GROUP_DESCRIPTOR * gd, EXT2_SUPER_BLOCK * sb, SECTOR numberOfSectors, UINT32 bytesPerSector);	//메모리 어떤 곳에 파일디스크립터 내용을 채워넣는 함수
+int create_root(DISK_OPERATIONS* disk, EXT2_SUPER_BLOCK * sb);						//루트 디렉터리를 생성하는 함수
+void process_meta_data_for_block_used(EXT2_FILESYSTEM * fs, UINT32 inode_num);		//
+typedef int(*EXT2_NODE_ADD)(EXT2_FILESYSTEM*,void*, EXT2_NODE*);					//
+
+#endif
---
+++ ext2_shell.c
@@ -0,0 +1,416 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include "ext2_shell.h"
+typedef struct {
+	char * address;
+}DISK_MEMORY;
+void printFromP2P(char * start, char * end);
+
+int fs_dumpDataSector(DISK_OPERATIONS* disk, int usedSector) // 섹터 단위로 데이터 출력
+{
+	char * start;
+	char * end;
+
+	start = ((DISK_MEMORY *)disk->pdata)->address + usedSector * disk->bytesPerSector; // 디스크에서 사용된 섹터의 시작주소부터
+	end = start + disk->bytesPerSector; // 마지막주소까지
+	printFromP2P(start, end); // 출력
+	printf("\n\n");
+
+	return EXT2_SUCCESS;
+}
+
+void printFromP2P(char * start, char * end)
+{
+	int start_int, end_int;
+	start_int = (int)start;
+	end_int = (int)end;
+
+	printf("start address : %#x , end address : %#x\n\n", start, end - 1);
+	start = (char *)(start_int &= ~(0xf));
+	end = (char *)(end_int |= 0xf);
+
+	// start부터 end까지 데이터 출력
+	while (start <= end)
+	{
+		if ((start_int & 0xf) == 0)
+			fprintf(stdout, "\n%#08x   ", start);
+
+		fprintf(stdout, "%02X  ", *(unsigned char *)start);
+		start++;
+		start_int++;
+	}
+	printf("\n\n");
+
+}
+void fs_dumpDataPart(DISK_OPERATIONS * disk, SHELL_FS_OPERATIONS * fsOprs, const SHELL_ENTRY * parent, SHELL_ENTRY * entry, const char * name)
+{
+	EXT2_NODE EXT2Parent;
+	EXT2_NODE EXT2Entry;
+	INODE node;
+	int result;
+	char * start, *end;
+
+	shell_entry_to_ext2_entry(parent, &EXT2Parent); // EXT2_ENTRY로 변환
+	if (result = ext2_lookup(&EXT2Parent, name, &EXT2Entry)) return result; // (ext2.c) 해당 이름을 가지는 엔트리의 위치를 찾아서 EXT2Entry에 저장
+
+	get_inode(EXT2Entry.fs, EXT2Entry.entry.inode, &node); // (ext2.c 구현X) inode number를 가진 파일의 INODE의 주소를 node에 저장
+
+	start = ((DISK_MEMORY *)disk->pdata)->address + (1 + node.block[0]) * disk->bytesPerSector; // ???
+	end = start + disk->bytesPerSector; // 섹터 단위
+	printFromP2P(start, end);
+}
+void fs_dumpfileinode(DISK_OPERATIONS * disk, SHELL_FS_OPERATIONS * fsOprs, const SHELL_ENTRY * parent, SHELL_ENTRY * entry, const char * name)
+{
+	EXT2_NODE EXT2Parent;
+	EXT2_NODE EXT2Entry;
+	INODE node;
+	int result, inode, i;
+
+
+	shell_entry_to_ext2_entry(parent, &EXT2Parent); // EXT2_ENTRY로 변환
+	if (result = ext2_lookup(&EXT2Parent, name, &EXT2Entry)) return result; // (ext2.c) 해당 이름을 가지는 엔트리의 위치를 찾아서 EXT2Entry에 저장
+
+	get_inode(EXT2Entry.fs, EXT2Entry.entry.inode, &node); // (ext2.c 구현X) inode number를 가진 파일의 INODE의 주소를 node에 저장
+	inode = EXT2Entry.entry.inode;
+
+	printf("inode number : %d\n", inode); // 검색한 inode number 출력
+	for (i = 0; i < EXT2_N_BLOCKS; i++) {
+		printf("iblock[%.2d] : %u\n", i, node.block[i]); // 해당 inode의 데이터 출력
+	}
+
+}
+void fs_dumpDataBlockByNum(DISK_OPERATIONS * disk, SHELL_FS_OPERATIONS * fsOprs, const SHELL_ENTRY * parent, SHELL_ENTRY * entry, int num)
+{
+	char * start, *end;
+
+	start = ((DISK_MEMORY *)disk->pdata)->address + (1 + num) * disk->bytesPerSector; // ???
+	end = start + disk->bytesPerSector; // 섹터 단위
+	printFromP2P(start, end);
+
+}
+void printf_by_sel(DISK_OPERATIONS* disk, SHELL_FS_OPERATIONS* fsOprs, const SHELL_ENTRY* parent, SHELL_ENTRY* entry, const char* name, int sel, int num)
+{
+	switch (sel) {
+	case 1: // super block 섹터단위 출력
+		fs_dumpDataSector(disk, 1);
+		break;
+	case 2: // group descriptor 섹터단위 출력
+		fs_dumpDataSector(disk, 2);
+		break;
+	case 3: // block bitmap 섹터단위 출력
+		fs_dumpDataSector(disk, 3);
+		break;
+	case 4: // inode bitmap 섹터단위 출력
+		fs_dumpDataSector(disk, 4);
+		break;
+	case 5: // inode table 섹터단위 출력
+		fs_dumpDataSector(disk, 5);
+		fs_dumpDataSector(disk, 6);
+		break;
+	case 6: // 해당 이름을 가진 data block을 섹터단위로 출력
+		fs_dumpDataPart(disk, fsOprs, parent, entry, name);
+		break;
+	case 7: // 해당 이름을 가진 파일의 inode 출력
+		fs_dumpfileinode(disk, fsOprs, parent, entry, name);
+		break;
+	case 8: // 해당 num을 가진 data block을 섹터단위로 출력
+		fs_dumpDataBlockByNum(disk, fsOprs, parent, entry, num);
+		break;
+	}
+}
+
+// 파티션을 해당 파일시스템으로 포맷
+int fs_format(DISK_OPERATIONS* disk, void* param)
+{
+	printf("formatting as a %s\n", (char *)param);
+	ext2_format(disk); // (ext2.c)
+
+	return  1;
+}
+
+static SHELL_FILE_OPERATIONS g_file =
+{
+	fs_create,
+	NULL,
+	NULL,
+	fs_write
+};
+
+static SHELL_FS_OPERATIONS   g_fsOprs =
+{
+	fs_read_dir,
+	fs_stat,
+	fs_mkdir,
+	fs_rmdir,
+	fs_lookup,
+
+	&g_file,
+	NULL
+};
+
+int fs_mount(DISK_OPERATIONS* disk, SHELL_FS_OPERATIONS* fsOprs, SHELL_ENTRY* root)
+{
+	EXT2_FILESYSTEM* fs;
+	EXT2_NODE ext2_entry;
+	int result;
+
+	*fsOprs = g_fsOprs; // g_fsOprs구조체에 EXT2오퍼레이션함수 등록
+
+	// EXT2_FILESYSTEM 구조체를 쉘 레벨 구조체에 연결 후 초기화
+	fsOprs->pdata = malloc(sizeof(EXT2_FILESYSTEM));
+	fs = FSOPRS_TO_EXT2FS(fsOprs); // fsOprs->pdata에 EXT2_FILESYSTEM 구조체 연결
+	ZeroMemory(fs, sizeof(EXT2_FILESYSTEM)); // 0으로 초기화
+	fs->disk = disk; // 디스크 등록
+
+	result = ext2_read_superblock(fs, &ext2_entry); // (ext2.c)
+
+	if (result == EXT2_SUCCESS) // ext2_read_superblock 수행 성공 시 설정된 값들 출력
+	{
+		printf("number of groups         : %d\n", NUMBER_OF_GROUPS);
+		printf("blocks per group         : %d\n", fs->sb.block_per_group);
+		printf("bytes per block          : %d\n", disk->bytesPerSector);
+		printf("free block count	 : %d\n", fs->sb.free_block_count);
+		printf("free inode count	 : %d\n", fs->sb.free_inode_count);
+		printf("first non reserved inode : %d\n", fs->sb.first_non_reserved_inode);
+		printf("inode structure size	 : %d\n", fs->sb.inode_structure_size);
+		printf("first data block number  : %d\n", fs->sb.first_data_block_each_group);
+		printf("\n----------------------------------------------\n");
+	}
+
+	printf("%s", ext2_entry.entry.name);
+	ext2_entry_to_shell_entry(fs, &ext2_entry, root); // SHELL_ENTRY로 변환
+
+	return result;
+}
+
+// (구현X) mount 해제
+void fs_umount(DISK_OPERATIONS* disk, SHELL_FS_OPERATIONS* fsOprs)
+{
+	return;
+}
+
+static SHELL_FILESYSTEM g_fat =
+{
+	"EXT2",
+	fs_mount,
+	fs_umount,
+	fs_format
+};
+
+// 엔트리 리스트에 새로운 엔트리 추가
+int adder(EXT2_FILESYSTEM* fs, void* list, EXT2_NODE* entry)
+{
+	SHELL_ENTRY_LIST*   entryList = (SHELL_ENTRY_LIST*)list;
+	SHELL_ENTRY         newEntry;
+
+	ext2_entry_to_shell_entry(fs, entry, &newEntry); // SHELL_ENTRY로 변환
+
+	add_entry_list(entryList, &newEntry); // ENTRY_LIST에 추가 (entrylist.c)
+
+	return EXT2_SUCCESS;
+}
+
+// 파일 쓰기
+int fs_write(DISK_OPERATIONS* disk, SHELL_FS_OPERATIONS* fsOprs, const SHELL_ENTRY* parent, SHELL_ENTRY* entry, unsigned long offset, unsigned long length, const char* buffer)
+{
+	EXT2_NODE EXT2Entry;
+
+	shell_entry_to_ext2_entry(entry, &EXT2Entry); // EXT2_ENTRY로 변환
+
+	return ext2_write(&EXT2Entry, offset, length, buffer); // (ext2.c) offset 위치부터 length만큼 buffer의 내용을 씀
+}
+
+// mount, umount, format 함수 포인터 지정
+void shell_register_filesystem(SHELL_FILESYSTEM* fs)
+{
+	*fs = g_fat;
+}
+
+// 파일 생성
+int	fs_create(DISK_OPERATIONS* disk, SHELL_FS_OPERATIONS* fsOprs, const SHELL_ENTRY* parent, const char* name, SHELL_ENTRY* retEntry)
+{
+	EXT2_NODE	EXT2Parent;
+	EXT2_NODE	EXT2Entry;
+	int				result;
+
+	shell_entry_to_ext2_entry(parent, &EXT2Parent); // EXT2_ENTRY로 변환
+
+	result = ext2_create(&EXT2Parent, name, &EXT2Entry); // (ext2.c) 파일명을 FAT_ENTRY 형식에 맞게 수정한 뒤 해당 이름을 가지는 엔트리가 존재하지 않으면 부모 디렉터리에 추가해줌
+
+	ext2_entry_to_shell_entry(EXT2Parent.fs, &EXT2Entry, retEntry); // SHELL_ENTRY로 변환
+
+	return result;
+}
+
+/*
+	precondition : shell_entry는 적절한 값으로 초기화 되어있는 상태이다.
+	postcondition : ext2_entry가 가리키는 공간에는 shell_entry에 대응하는 ext2_entry가 저장되어 있다.
+*/
+int shell_entry_to_ext2_entry(const SHELL_ENTRY* shell_entry, EXT2_NODE* ext2_entry) // SHELL_ENTRY를 EXT2_ENTRY로 변환
+{
+	EXT2_NODE* entry = (EXT2_NODE*)shell_entry->pdata; // 엔트리 보관 버퍼
+
+	*ext2_entry = *entry; // 데이터 저장
+
+	return EXT2_SUCCESS;
+}
+
+/*
+	postcondition : shell_entry는 shell level directory entry를 갖고 동시에 ext2 directory entry를
+	pdata 버퍼에 보관한다. 즉, shell level에서는 언제든 양쪽 entry에 접근이 가능해진다.
+*/
+int ext2_entry_to_shell_entry(EXT2_FILESYSTEM* fs, const EXT2_NODE* ext2_entry, SHELL_ENTRY* shell_entry) // EXT2_ENTRY를 SHELL_ENTRY로 변환
+{
+	EXT2_NODE* entry = (EXT2_NODE*)shell_entry->pdata; // 엔트리 보관 버퍼
+	INODE inodeBuffer;
+	BYTE* str = "/";
+
+	ZeroMemory(shell_entry, sizeof(SHELL_ENTRY)); // 데이터를 저장할 영역을 0으로 초기화
+
+	int inode = ext2_entry->entry.inode; // inode number
+	int result = get_inode(fs, inode, &inodeBuffer); // (ext2.c 구현X) inode number를 가진 파일의 INODE주소를 inodeBuffer에 저장 
+
+	if (ext2_entry->entry.name[0] != '.' && inode == 2); // 현재 디렉터리가 아닌 경우
+	else {
+		str = shell_entry->name;
+		str = my_strncpy(str, ext2_entry->entry.name, 8); // 이름 설정
+		if (ext2_entry->entry.name[8] != 0x20) // 확장자가 있는 경우
+		{
+			str = my_strncpy(str, ".", 1);
+			str = my_strncpy(str, &ext2_entry->entry.name[8], 3); // 확장자 설정
+		}
+	}
+	if (FILE_TYPE_DIR & inodeBuffer.mode) // 디렉터리인 경우
+		shell_entry->isDirectory = 1; // 디렉터리라고 설정
+	else // 파일인 경우
+		shell_entry->isDirectory = 0;
+
+	shell_entry->permition = 0x01FF & inodeBuffer.mode; // 파일의 접근 제어 정보 저장
+
+	shell_entry->size = inodeBuffer.size; // 파일의 사이즈 저장
+
+	*entry = *ext2_entry; // 데이터 저장
+
+	return EXT2_SUCCESS;
+}
+
+// 해당 이름을 가지는 엔트리의 위치를 찾음
+int fs_lookup(DISK_OPERATIONS* disk, SHELL_FS_OPERATIONS* fsOprs, const SHELL_ENTRY* parent, SHELL_ENTRY* entry, const char* name)
+{
+	EXT2_NODE	EXT2Parent;
+	EXT2_NODE	EXT2Entry;
+	int				result;
+
+	shell_entry_to_ext2_entry(parent, &EXT2Parent); // EXT2_ENTRY로 변환
+
+	if (result = ext2_lookup(&EXT2Parent, name, &EXT2Entry))return result; // (ext2.c) 해당 이름을 가지는 엔트리의 위치를 찾아서 EXT2Entry에 저장
+
+	ext2_entry_to_shell_entry(EXT2Parent.fs, &EXT2Entry, entry); // SHELL_ENTRY로 변환
+
+	return result;
+}
+
+// 디렉터리의 엔트리들을 리스트에 담음
+int fs_read_dir(DISK_OPERATIONS* disk, SHELL_FS_OPERATIONS* fsOprs, const SHELL_ENTRY* parent, SHELL_ENTRY_LIST* list)
+{
+	EXT2_NODE   entry;
+
+	if (list->count)
+		release_entry_list(list); // 리스트 삭제 (entrylist.c)
+
+	shell_entry_to_ext2_entry(parent, &entry); // EXT2_ENTRY로 변환
+	ext2_read_dir(&entry, adder, list); // 디렉터리의 엔트리들을 리스트에 담음
+
+	return EXT2_SUCCESS;
+}
+
+// 문자열 비교
+int my_strnicmp(const char* str1, const char* str2, int length)
+{
+	char   c1, c2;
+
+	// length만큼 둘 다 공백이 아닌동안 대문자로 바꿔서 비교 후 같으면 0을 리턴
+	while (((*str1 && *str1 != 0x20) || (*str2 && *str2 != 0x20)) && length-- > 0)
+	{
+		c1 = toupper(*str1);
+		c2 = toupper(*str2);
+
+		if (c1 > c2)
+			return -1;
+		else if (c1 < c2)
+			return 1;
+
+		str1++;
+		str2++;
+	}
+
+	return 0;
+}
+
+// 현재 디렉터리에 해당 이름의 엔트리가 존재하는지 검사
+int is_exist(DISK_OPERATIONS* disk, SHELL_FS_OPERATIONS* fsOprs, const SHELL_ENTRY* parent, const char* name)
+{
+	SHELL_ENTRY_LIST      list;
+	SHELL_ENTRY_LIST_ITEM*   current;
+
+	init_entry_list(&list); // ENTRY_LIST를 초기화 (entrylist.c)
+
+	fs_read_dir(disk, fsOprs, parent, &list); // 디렉터리의 엔트리들을 리스트에 담음
+	current = list.first; // 리스트의 첫 엔트리부터
+
+	while (current) // 마지막 엔트리까지 같은 이름이 있는지 검사
+	{
+		if (my_strnicmp((char*)current->entry.name, name, 12) == 0) // 있으면
+		{
+			release_entry_list(&list); // ENTRY_LIST 삭제 후 에러 리턴 (entrylist.c)
+			return EXT2_ERROR;
+		}
+
+		current = current->next; // 다음 엔트리
+	}
+
+	release_entry_list(&list); // ENTRY_LIST 삭제 후 성공 리턴
+	return EXT2_SUCCESS;
+}
+
+// 디렉터리 생성
+int fs_mkdir(DISK_OPERATIONS* disk, SHELL_FS_OPERATIONS* fsOprs, const SHELL_ENTRY* parent, const char* name, SHELL_ENTRY* retEntry)
+{
+	EXT2_FILESYSTEM* ext2;
+	EXT2_NODE      EXT2_Parent;
+	EXT2_NODE      EXT2_Entry;
+	int               result;
+
+	ext2 = (EXT2_FILESYSTEM*)fsOprs->pdata; // EXT2_FILESYSTEM 데이터
+
+	if (is_exist(disk, fsOprs, parent, name)) // 현재 디렉터리에 해당 이름의 엔트리가 있는지 검사
+		return EXT2_ERROR;
+
+	shell_entry_to_ext2_entry(parent, &EXT2_Parent); // EXT2_ENTRY로 변환
+
+	result = ext2_mkdir(&EXT2_Parent, name, &EXT2_Entry); // (ext2.c) 디렉터리를 생성하고 '.'와 '..'을 추가
+
+	ext2_entry_to_shell_entry(ext2, &EXT2_Entry, retEntry); // SHELL_ENTRY로 변환
+
+	return result;
+}
+
+int fs_stat(DISK_OPERATIONS* disk, SHELL_FS_OPERATIONS* fsOprs, unsigned int total, unsigned int used)
+{
+	/*
+	shell.c의 shell_cmd_df에서는 free sectors, used sectors 출력
+ 	shell.c에서는 위의 인자들을 넣어 호출하고 있는데 첫 번째 인자인 DISK_OPERATIONS는 빼도 될 듯 -> 쓸 데가 없음
+ 	두 번째 인자로 받은 SHELL_FS_OPERATIONS의 pdata는 EXT2_FILESYSTEM을 가르키고 있음    
+	-> ext2_df를 호출하면서 인자로는 이 EXT2_FILESYSTEM과 total, used를 넘겨줌
+	*/
+}
+
+int fs_rmdir(DISK_OPERATIONS* disk, SHELL_FS_OPERATIONS* fsOprs, SHELL_ENTRY* parent, const char* name)
+{
+	/*
+	이 함수도 DISK_OPERATIONS를 왜 넘겨주는지 모르겠음(사용 안하는 것 같음)
+ 	인자로 받은 parent를 shell_entry_to_ext2_entry 함수로 ext_entry로 변환, 
+	ext2_lookup으로 해당 엔트리 찾은 후 그 엔트리의 주소를 ext2_rmdir에 인자로 전달
+	*/
+}
